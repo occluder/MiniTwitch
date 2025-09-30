38dba835c794c2e16217f8c16375f91ba8eee3c8
Elias A. <69414142+occluder@users.noreply.github.com>
2025-09-30 10:57:30 +0300

    Add `Get Clips Download` Endpoint (#182)
    * Add `Get Clips Download` Endpoint
    
    * Add test

---
+++ MiniTwitch.Helix/Responses/ClipsDownload.cs
@@ -0,0 +1,12 @@
+﻿using MiniTwitch.Helix.Models;
+
+namespace MiniTwitch.Helix.Responses;
+
+public class ClipsDownload : BaseResponse<ClipsDownload.ClipDownload>
+{
+    public record ClipDownload(
+        string ClipId,
+        string? LandscapeDownloadUrl,
+        string? PortraitDownloadUrl
+    );
+}
